88f949ed32aa957c9e228c26e274d65b03c1db3f
Tilek <timur.almamatov@yandex.ru>
2026-04-02 21:58:07 +0700

    закомитил пока не сломал в кафе

--- front/src/pages/AdviewPage/AdViewPage.tsx
+++ front/src/pages/AdviewPage/AdViewPage.tsx
@@ -8,14 +8,19 @@ const AdViewPage = () => {
         <div>MacBook Pro 16”</div>
         <div>64000 ₽</div>
       </div>
-      <div>
+      <div className="flex justify-between">
         <Button
           type="primary"
+          style={{ backgroundColor: '#1890FF', borderColor: '#1890FF', fontSize: '22px' }}
           icon={<EditOutlined />}
-          style={{ backgroundColor: '#1890FF', borderColor: '#1890FF' }}
+          iconPlacement="end"
         >
           Редактировать
         </Button>
+        <div className="text-[#848388] flex flex-col text-right text-[19px]">
+          <div>Опубликовано: 10 марта 22:39</div>
+          <div>Отредактировано: 10 марта 23:12</div>
+        </div>
       </div>
     </div>
   );
