348e996f8f84f994c6cc6d8febd0a9ba832df578
Tilek <timur.almamatov@yandex.ru>
2026-04-06 05:46:28 +0700

    исправил докер

--- server/server.ts
+++ server/server.ts
@@ -168,9 +168,10 @@ fastify.put<ItemUpdateRequest>('/items/:id', (request, reply) => {
   }
 });
 
+
 const port = Number(process.env.port) || 8080;
 
-fastify.listen({ port }, function (err, _address) {
+fastify.listen({ port, host: '0.0.0.0' }, function (err, _address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
